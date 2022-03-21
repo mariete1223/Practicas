aefd1550dda3d2fbed7ce57f94a75cd6b9f0b2a4
mariete1223 <71662461+mariete1223@users.noreply.github.com>
2022-03-21 17:43:12 +0100

    Delete ProyectoSpring/src directory

--- ProyectoSpring/src/main/java/net/acc/customproyect/repositories/PacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package net.acc.customproyect.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.acc.customproyect.entities.Paciente;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Long>{
-
-}
